abdb825e296dc8b4ff27e312f3dbfd5f27595b29
Adam Ginsburg <keflavich@gmail.com>
2013-08-20 21:45:37 -0600

    more log updates!  yay.

--- cband/linemaps_cband.py
+++ cband/linemaps_cband.py
@@ -25,24 +25,3 @@ clean(vis='W51Ku_sp7_split.ms.contsub',imagename=imagename+'_contsub',field='W51
         weighting='briggs', robust=0.5, niter=10000, spw='0',
         restfreq='6.6685192GHz')
 exportfits(imagename=imagename+".image", fitsimage=imagename+".fits", overwrite=True)
-#13A-064.sb18020284.eb19181492.56353.71736577546.ms
-#W51Ku_spw0_AVG.ms
-#W51Ku_spw10_AVG.ms
-#W51Ku_spw11_AVG.ms
-#W51Ku_spw12_AVG.ms
-#W51Ku_spw13_AVG.ms
-#W51Ku_spw14_AVG.ms
-#W51Ku_spw15_AVG.ms
-#W51Ku_spw16_AVG.ms
-#W51Ku_spw17_AVG.ms
-#W51Ku_spw18_AVG.ms
-#W51Ku_spw19_AVG.ms  H2CO
-#W51Ku_spw1_AVG.ms   12.1 ghz maser
-#W51Ku_spw2_AVG.ms
-#W51Ku_spw3_AVG.ms
-#W51Ku_spw4_AVG.ms
-#W51Ku_spw5_AVG.ms
-#W51Ku_spw6_AVG.ms
-#W51Ku_spw7_AVG.ms
-#W51Ku_spw8_AVG.ms
-#W51Ku_spw9_AVG.ms
