de09d368d3d9d6a1e6c9ef532793cfde738f57b4
Adam Ginsburg <keflavich@gmail.com>
2013-08-22 21:06:57 -0600

    added spw0 script for 3c48

--- 3c48_script.py
+++ 3c48_script.py
@@ -1,29 +1,30 @@
 rawvis='13A-064.sb18020284.eb19181492.56353.71736577546.ms'
 vis = '3c48_Ku.ms'
 flux_calibrator = '0137+331=3C48'
+refant='ea04'
 split(vis=rawvis, outputvis=vis, field=flux_calibrator, datacolumn='data', spw='')
 setjy(vis=vis, field=flux_calibrator, scalebychan=True, modimage='3C48_C.im', usescratch=False)
 gencal(vis=vis,caltable='calKu.gaincurve',caltype='gc')
 gaincal(vis=vis,caltable='calKu.G0',field=flux_calibrator, spw='',
     gaintable=['calKu.gaincurve'],
-    gaintype='G',refant='', calmode='p',solint='int',minsnr=3,gaincurve=False)
+    gaintype='G',refant=refant, calmode='p',solint='int',minsnr=3,gaincurve=False)
 plotcal(caltable='calKu.G0',xaxis='time',yaxis='phase', figfile='plots/3c48_phasecal_phase.png')
 gaincal(vis=vis,caltable='calKu.K0',
     gaintable=['calKu.gaincurve','calKu.G0'],
     field=flux_calibrator,spw='',gaintype='K',gaincurve=False,
-    refant='',combine='scan',solint='inf',minsnr=3)
+    refant=refant,combine='scan',solint='inf',minsnr=3)
 plotcal(caltable='calKu.G0',xaxis='time',yaxis='phase', figfile='plots/3c48_ampcal_phase.png')
 plotcal(caltable='calKu.G0',xaxis='time',yaxis='amp', figfile='plots/3c48_ampcal_amp.png')
 bandpass(vis=vis,caltable='calKu.B0',
     gaintable=['calKu.gaincurve', 'calKu.G0','calKu.K0'],
-    field=flux_calibrator,refant='',solnorm=False,
+    field=flux_calibrator,refant=refant,solnorm=False,
     bandtype='B', combine='scan', solint='inf', gaincurve=False)
 plotcal(caltable='calKu.B0',xaxis='freq',yaxis='amp', figfile='plots/3c48_bandcal_amp.png')
 plotcal(caltable='calKu.B0',xaxis='freq',yaxis='phase', figfile='plots/3c48_bandcal_phase.png')
 gaincal(vis=vis,caltable='calKu.G1int',
     gaintable=['calKu.gaincurve',
               'calKu.K0','calKu.B0'],
-    field=flux_calibrator,refant='',solnorm=F,
+    field=flux_calibrator,refant=refant,solnorm=F,
     spw='', solint='int',gaintype='G',calmode='p',gaincurve=False,)
 plotcal(caltable='calKu.G1int',xaxis='time',yaxis='phase', figfile='plots/3c48_G1cal_phase.png')
 plotcal(caltable='calKu.G1int',xaxis='time',yaxis='amp', figfile='plots/3c48_G1cal_amp.png')
@@ -33,7 +34,7 @@ gaincal(vis=vis, caltable='calKu.G2',
                    'calKu.K0','calKu.B0','calKu.G1int'], 
         gainfield=['','',flux_calibrator,flux_calibrator,flux_calibrator], 
         interp=['','','nearest','nearest','nearest'], 
-        field=flux_calibrator,refant='',solnorm=F,
+        field=flux_calibrator,refant=refant,solnorm=F,
         spw='',
         solint='inf',combine='scan',gaintype='G',calmode='a',gaincurve=False)
 gaincal(vis=vis, caltable='calKu.G3', 
@@ -41,7 +42,7 @@ gaincal(vis=vis, caltable='calKu.G3',
                    'calKu.K0','calKu.B0','calKu.G1int'], 
         gainfield=['','',flux_calibrator,flux_calibrator,flux_calibrator], 
         interp=['','','nearest','nearest','nearest'], 
-        field=flux_calibrator,refant='',solnorm=F,
+        field=flux_calibrator,refant=refant,solnorm=F,
         spw='',
         solint='inf',combine='scan',gaintype='G',calmode='a',gaincurve=False)
 
---
+++ 3c48_spw0_script.py
@@ -0,0 +1,55 @@
+rawvis='13A-064.sb18020284.eb19181492.56353.71736577546.ms'
+vis = '3c48_Ku_spw0.ms'
+flux_calibrator = '0137+331=3C48'
+refant='ea03'
+split(vis=rawvis, outputvis=vis, field=flux_calibrator, datacolumn='data', spw='0')
+setjy(vis=vis, field=flux_calibrator, scalebychan=True, modimage='3c48_spw0_C.im', usescratch=False)
+gencal(vis=vis,caltable='calKu_spw0.gaincurve',caltype='gc')
+gaincal(vis=vis,caltable='calKu_spw0.G0',field=flux_calibrator, spw='',
+    gaintable=['calKu_spw0.gaincurve'],
+    gaintype='G',refant=refant, calmode='p',solint='int',minsnr=3,gaincurve=False)
+plotcal(caltable='calKu_spw0.G0',xaxis='time',yaxis='phase', figfile='plots/3c48_spw0_phasecal_phase.png')
+gaincal(vis=vis,caltable='calKu_spw0.K0',
+    gaintable=['calKu_spw0.gaincurve','calKu_spw0.G0'],
+    field=flux_calibrator,spw='',gaintype='K',gaincurve=False,
+    refant=refant,combine='scan',solint='inf',minsnr=3)
+plotcal(caltable='calKu_spw0.G0',xaxis='time',yaxis='phase', figfile='plots/3c48_spw0_ampcal_phase.png')
+plotcal(caltable='calKu_spw0.G0',xaxis='time',yaxis='amp', figfile='plots/3c48_spw0_ampcal_amp.png')
+bandpass(vis=vis,caltable='calKu_spw0.B0',
+    gaintable=['calKu_spw0.gaincurve', 'calKu_spw0.G0','calKu_spw0.K0'],
+    field=flux_calibrator,refant=refant,solnorm=False,
+    bandtype='B', combine='scan', solint='inf', gaincurve=False)
+plotcal(caltable='calKu_spw0.B0',xaxis='freq',yaxis='amp', figfile='plots/3c48_spw0_bandcal_amp.png')
+plotcal(caltable='calKu_spw0.B0',xaxis='freq',yaxis='phase', figfile='plots/3c48_spw0_bandcal_phase.png')
+gaincal(vis=vis,caltable='calKu_spw0.G1int',
+    gaintable=['calKu_spw0.gaincurve',
+              'calKu_spw0.K0','calKu_spw0.B0'],
+    field=flux_calibrator,refant=refant,solnorm=F,
+    spw='', solint='int',gaintype='G',calmode='p',gaincurve=False,)
+plotcal(caltable='calKu_spw0.G1int',xaxis='time',yaxis='phase', figfile='plots/3c48_spw0_G1cal_phase.png')
+plotcal(caltable='calKu_spw0.G1int',xaxis='time',yaxis='amp', figfile='plots/3c48_spw0_G1cal_amp.png')
+
+gaincal(vis=vis, caltable='calKu_spw0.G2', 
+        gaintable=['calKu_spw0.gaincurve',
+                   'calKu_spw0.K0','calKu_spw0.B0','calKu_spw0.G1int'], 
+        gainfield=['','',flux_calibrator,flux_calibrator,flux_calibrator], 
+        interp=['','','nearest','nearest','nearest'], 
+        field=flux_calibrator,refant=refant,solnorm=F,
+        spw='',
+        solint='inf',combine='scan',gaintype='G',calmode='a',gaincurve=False)
+gaincal(vis=vis, caltable='calKu_spw0.G3', 
+        gaintable=['calKu_spw0.gaincurve',
+                   'calKu_spw0.K0','calKu_spw0.B0','calKu_spw0.G1int'], 
+        gainfield=['','',flux_calibrator,flux_calibrator,flux_calibrator], 
+        interp=['','','nearest','nearest','nearest'], 
+        field=flux_calibrator,refant=refant,solnorm=F,
+        spw='',
+        solint='inf',combine='scan',gaintype='G',calmode='a',gaincurve=False)
+
+plotcal(caltable='calKu_spw0.G2int',xaxis='time',yaxis='phase', figfile='plots/3c48_spw0_G2cal_phase.png')
+plotcal(caltable='calKu_spw0.G2int',xaxis='time',yaxis='amp', figfile='plots/3c48_spw0_G2cal_amp.png')
+plotcal(caltable='calKu_spw0.G3int',xaxis='time',yaxis='phase', figfile='plots/3c48_spw0_G3cal_phase.png')
+plotcal(caltable='calKu_spw0.G3int',xaxis='time',yaxis='amp', figfile='plots/3c48_spw0_G3cal_amp.png')
+
+clean(vis=vis, imagename='3c48_spw0_Ku', imsize=1024)
+
