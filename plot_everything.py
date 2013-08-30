cddb195ad00b7aaa32eeacc82128e789d94ab02f
Adam Ginsburg <keflavich@gmail.com>
2013-08-30 13:08:49 -0600

    plot everything, apparently

--- plot_everything.py
+++ plot_everything.py
@@ -1,9 +1,9 @@
 import itertools
 from collections import defaultdict
 
-def plot_everything(vis, ydatacolumn='data', async=True):
+def plot_everything(vis, ydatacolumn='data', async=False):
 
-    xaxes = ['time','frequency','antenna','baseline','uvdist']
+    xaxes = ['frequency','antenna','baseline','uvdist','time',]
     yaxes = ['phase','amp']
 
 
@@ -12,14 +12,15 @@ def plot_everything(vis, ydatacolumn='data', async=True):
     d_avgtime = defaultdict(lambda:'60s')
     d_avgtime['time'] = ''
 
-    d_coloraxis = {'time': 'antenna1', 'frequency': 'antenna1', 'antenna':'scan',
-                 'baseline':'scan', 'uvdist':'scan'}
+    d_coloraxis = {'time': 'antenna1', 'frequency': 'antenna1', 'antenna':'corr',
+                 'baseline':'corr', 'uvdist':'corr'}
 
     for x,y in itertools.product(xaxes,yaxes):
         plotfile = '{ms}_{dc}_{x}_vs_{y}.png'.format(
                     ms=vis.replace(".ms",""), x=x, y=y,
                     dc=ydatacolumn)
-        print "Attempting to plot %s vs %s with outfile %s" % (x,y,plotfile)
+        #print "Attempting to plot %s vs %s with outfile %s" % (x,y,plotfile)
+        #print "Plot command: "
         plotms(vis=vis,xaxis=x,yaxis=y,
                 avgchannel=d_avgchannel[x],
                 avgtime=d_avgtime[x],
@@ -27,4 +28,35 @@ def plot_everything(vis, ydatacolumn='data', async=True):
                 async=async,
                 ydatacolumn=ydatacolumn,
                 plotfile=plotfile,
+                overwrite=True,
+                interactive=True,
+                expformat='png',
+                highres=True,
                 )
+        plotxy(vis=vis, xaxis=x, yaxis=y, datacolumn=ydatacolumn,
+               timebin=d_avgtime[x].strip('s'),
+               width=d_avgchannel[x],
+               multicolor=d_coloraxis[x],
+               async=async,
+               figfile=plotfile,
+               interactive=True)
+
+        print """plotms(vis='{vis}',xaxis='{xaxis}',yaxis='{yaxis}',
+                avgchannel='{avgchannel}',
+                avgtime='{avgtime}',
+                coloraxis='{coloraxis}',
+                async={async},
+                ydatacolumn='{ydatacolumn}',
+                plotfile='{plotfile}',
+                overwrite=True,
+                interactive=True,
+                )""".format(**dict(vis=vis,xaxis=x,yaxis=y,
+                avgchannel=d_avgchannel[x],
+                avgtime=d_avgtime[x],
+                coloraxis=d_coloraxis[x],
+                async=async,
+                ydatacolumn=ydatacolumn,
+                plotfile=plotfile,
+                expformat='png',
+                highres=True,
+                ))
