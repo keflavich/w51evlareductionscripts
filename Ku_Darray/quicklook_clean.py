6e6df829a1c3f859cde105a90b6803485c6c9195
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-11-11 15:19:51 +0100

    plot_everything is a global script

--- Ku_Darray/quicklook_clean.py
+++ Ku_Darray/quicklook_clean.py
@@ -1,3 +1,12 @@
+"""
+11/11/2013:
+    "Quick Look" mapping of the D-array data so that everything can be inspected in FITS form
+"""
+vis = '13A-064.sb18020284.eb19181492.56353.71736577546.ms'
+
+# imsize = 256, cellsize = 0.5 arcsec for D-array
+# to match
+# imsize = 512, cellsize = 0.25 arcsec for B-array
 for ii in xrange(23):
     clean(vis=vis, field='W51 Ku', spw=str(ii),
             imagename='W51Ku_Darray_spw%02i.clean' % ii, psfmode='hogbom',
