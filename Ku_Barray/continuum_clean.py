8459646fd34ba5b8ae17b973dee92affac3c1728
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-11-19 13:37:30 +0100

    continuum clean

--- Ku_Barray/continuum_clean.py
+++ Ku_Barray/continuum_clean.py
@@ -5,32 +5,36 @@
 """
 vis = '13A-064.sb24208616.eb26783844.56566.008853900465.ms'
 
-clean(vis=vis,
-      field='W51 Ku',
-      spw='2,3,4,5,6,7', # left out 0,8
-      imagename='W51Ku_Barray_continuum_low.hires.clean',
-      psfmode='hogbom',
-      cell=['0.15 arcsec'],
-      imsize=[768,768],
-      niter=50000,
-      threshold='0.01 mJy',
-      mode='mfs',
-      multiscale=[0,5,10],
-      outframe='LSRK',
-      pbcor=T)
-exportfits('W51Ku_Barray_continuum_low.hires.clean.image','W51Ku_Barray_continuum_low.hires.clean.image.fits',overwrite=True)
-clean(vis=vis,
-      field='W51 Ku',
-      spw='12,13,14,16,17,18', # left out 10,20
-      imagename='W51Ku_Barray_continuum_high.hires.clean',
-      psfmode='hogbom',
-      cell=['0.15 arcsec'],
-      imsize=[768,768],
-      niter=50000,
-      threshold='0.01 mJy',
-      mode='mfs',
-      multiscale=[0,5,10],
-      outframe='LSRK',
-      pbcor=T)
-exportfits('W51Ku_Barray_continuum_high.hires.clean.image','W51Ku_Barray_continuum_high.hires.clean.image.fits',overwrite=True)
+# Low: left out 0,8
+# High: left out 10,20
+low,high = '2,3,4,5,6,7','12,13,14,16,17,18'
+niter = {'dirty':0, 'clean':50000}
 
+def myclean(spw, name, dirtyclean='dirty', multiscale=[0,3,6,8,10,15,30]):
+
+    imagename = 'W51Ku_Barray_continuum_%s.hires.%s' % (name,dirtyclean)
+
+    print "Cleaning image ",imagename
+
+    clean(vis=vis,
+          field='W51 Ku',
+          spw=spw, 
+          imagename=imagename,
+          psfmode='hogbom',
+          cell=['0.15 arcsec'],
+          imsize=[768,768],
+          niter=niter[dirtyclean],
+          threshold='0.01 mJy',
+          mode='mfs',
+          multiscale=multiscale,
+          outframe='LSRK',
+          pbcor=T,
+          usescratch=True)
+
+    print "Exporting image ",imagename
+    exportfits(imagename,imagename+'.fits',overwrite=True)
+
+myclean(low,'low','dirty')
+myclean(low,'high','dirty')
+myclean(low,'low','clean')
+myclean(low,'high','clean')
