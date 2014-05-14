2bd0d3ee0c2a899b0bad69cedf378ceb4c1c64fb
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-05-14 07:26:07 +0200

    OLD VERSION of single channel clean

--- C_Aarray/single_channel_example_ch3ohselfcal.py
+++ C_Aarray/single_channel_example_ch3ohselfcal.py
@@ -1,63 +1,104 @@
-"""
-Test script run on May 13 2014
-"""
-outputvis = 'ch3oh_selfcal_test'
-if not os.path.exists(vis):
-    split(vis=vis, outputvis=outputvis, datacolumn='corrected',
-          spw='7:632', field='W51 Ku',
-          timerange='2014/04/19/13:38:11~2014/04/19/14:38:11',
-          keepflags=True)
+split(vis=vis, outputvis='ch3oh_selfcal_test', datacolumn='corrected',
+      spw='7:632', field='W51 Ku',
+      timerange='2014/04/19/13:38:11~2014/04/19/14:38:11',
+      keepflags=True)
 
 
-# first model, I guess
-vis = outputvis
-flagdata(vis=vis, field='W51 Ku', spw='0', mode='unflag')
-delmod(vis=vis)
+vis='ch3oh_selfcal_test'
 clean(vis=vis, spw='0', imagename='ch3oh_1024_chan632',
       field='W51 Ku',
-      weighting='uniform', imsize=[2048,2048], cell=['0.1 arcsec'],
+      weighting='uniform', imsize=[1024,1024], cell=['0.1 arcsec'],
       mode='mfs', threshold='1 mJy', niter=1000,
       selectdata=True)
-exportfits('ch3oh_2048_chan632.image','ch3oh_2048_chan632.image.fits')
-
-solint = '5s'
+exportfits('ch3oh_1024_chan632.image','ch3oh_1024_chan632.image.fits')
 
 #delmod(vis=vis)
 #setjy(vis=vis, model='ch3oh_1024_chan632.image')
-for ii in range(5):
-    caltable = 'ch3oh_selfcal_phase{0:d}'.format(ii)
-    gaincal(vis=vis,
-            field='',
-            caltable=caltable,
-            spw='',
-            # gaintype = 'T' could reduce failed fit errors by averaging pols...
-            gaintype='G', #  'G' from http://casaguides.nrao.edu/index.php?title=EVLA_Advanced_Topics_3C391
-            solint=solint,
-            refant='ea14',
-            calmode='p',
-            combine='scan',
-            minsnr=3,
-            minblperant=4)
-    plotcal(caltable, yaxis='phase')
-    applycal(vis=vis,
-             gaintable=caltable,
-             interp='linear',
-             flagbackup=True)
+caltable = 'ch3oh_selfcal_phase'
+gaincal(vis=vis,
+        field='',
+        caltable=caltable,
+        spw='',
+        # gaintype = 'T' could reduce failed fit errors by averaging pols...
+        gaintype='T', #  'G' from http://casaguides.nrao.edu/index.php?title=EVLA_Advanced_Topics_3C391
+        solint='int',
+        refant='ea14',
+        calmode='p',
+        combine='scan',
+        minsnr=3,
+        minblperant=4)
+plotcal(caltable, yaxis='phase') # looks great!
+applycal(vis=vis,
+         gaintable=caltable,
+         interp='linear',
+         flagbackup=True) # was False when flagmanager was used
 
-    os.system('rm -rf ch3oh_2048_chan632_selfcal{0:d}.*'.format(ii))
+clean(vis=vis, spw='0', imagename='ch3oh_1024_chan632_selfcal',
+      field='W51 Ku',
+      weighting='uniform', imsize=[1024,1024], cell=['0.1 arcsec'],
+      mode='mfs', threshold='1 mJy', niter=1000,
+      selectdata=True)
+exportfits('ch3oh_1024_chan632_selfcal.image','ch3oh_1024_chan632_selfcal.image.fits')
+# shows improvement, but maybe only mild improvement
 
-    clean(vis=vis, spw='0', imagename='ch3oh_1024_chan632_selfcal{0:d}'.format(ii),
-          field='W51 Ku',
-          weighting='uniform', imsize=[1024,1024], cell=['0.1 arcsec'],
-          mode='mfs', threshold='10 mJy', niter=1000,
-          selectdata=True)
-    exportfits('ch3oh_2048_chan632_selfcal{0:d}.image'.format(ii),
-               'ch3oh_2048_chan632_selfcal{0:d}.image.fits'.format(ii))
+# clear flags because some points were flagged for low SNR
+flagdata(vis=vis, field='W51 Ku', spw='0', mode='unflag')
+caltable = 'ch3oh_selfcal2_phase'
+gaincal(vis=vis,
+        field='',
+        caltable=caltable,
+        spw='',
+        # gaintype = 'T' could reduce failed fit errors by averaging pols...
+        gaintype='T', #  'G' from http://casaguides.nrao.edu/index.php?title=EVLA_Advanced_Topics_3C391
+        solint='5s', # increase solint to reduce flagging.  Phase is definitely stable on these scales
+        refant='ea14',
+        calmode='p',
+        combine='scan',
+        minsnr=3,
+        minblperant=4)
+plotcal(caltable, yaxis='phase') # looks good, but there is oddity at late times (ea18)
+applycal(vis=vis,
+         gaintable=caltable,
+         interp='linear',
+         flagbackup=True) # was False when flagmanager was used
+flagdata(vis=vis, mode='manual', antenna='ea18')
+flagdata(vis=vis, mode='clip', clipzeros=True)
+rm -rf ch3oh_1024_chan632_selfcal2.*
+clean(vis=vis, spw='0', imagename='ch3oh_1024_chan632_selfcal2',
+      field='W51 Ku',
+      weighting='uniform', imsize=[1024,1024], cell=['0.1 arcsec'],
+      mode='mfs', threshold='10 mJy', niter=10000,
+      selectdata=True)
+exportfits('ch3oh_1024_chan632_selfcal2.image','ch3oh_1024_chan632_selfcal2.image.fits')
 
-phasecaltable = 'ch3oh_selfcal_phase{0:d}'.format(ii)
-ii += 1
-vis='ch3oh_selfcal_test'
-caltable = 'ch3oh_selfcal_ampphase'
+caltable = 'ch3oh_selfcal3_phase'
+gaincal(vis=vis,
+        field='',
+        caltable=caltable,
+        spw='',
+        # gaintype = 'T' could reduce failed fit errors by averaging pols...
+        gaintype='T', #  'G' from http://casaguides.nrao.edu/index.php?title=EVLA_Advanced_Topics_3C391
+        solint='5s', # increase solint to reduce flagging.  Phase is definitely stable on these scales
+        refant='ea14',
+        calmode='p',
+        combine='scan',
+        minsnr=3,
+        minblperant=4)
+plotcal(caltable, yaxis='phase') # seems fine?
+applycal(vis=vis,
+         gaintable=caltable,
+         interp='linear',
+         flagbackup=True) # was False when flagmanager was used
+rm -rf ch3oh_1024_chan632_selfcal3.*
+clean(vis=vis, spw='0', imagename='ch3oh_2048_chan632_selfcal3',
+      field='W51 Ku',
+      weighting='uniform', imsize=[2048,2048], cell=['0.1 arcsec'],
+      mode='mfs', threshold='10 mJy', niter=10000,
+      selectdata=True)
+exportfits('ch3oh_2048_chan632_selfcal3.image','ch3oh_2048_chan632_selfcal3.image.fits')
+
+phasecaltable = 'ch3oh_selfcal3_phase'
+caltable = 'ch3oh_selfcal4_ampphase'
 gaincal(vis=vis,
         field='',
         caltable=caltable,
@@ -70,15 +111,13 @@ gaincal(vis=vis,
         combine='scan',
         minsnr=3,
         minblperant=4)
-flagdata(vis=vis, field='W51 Ku', spw='0', mode='unflag')
-plotcal(caltable, yaxis='phase', xaxis='amp')
-# Miller said to apply both
+plotcal(caltable, yaxis='phase')
 applycal(vis=vis,
          gaintable=[phasecaltable,caltable],
          interp='linear',
          flagbackup=True) # was False when flagmanager was used
-os.system('rm -rf ch3oh_2048_chan632_selfcal{0:d}.*'.format(ii))
-clean(vis=vis, spw='0', imagename='ch3oh_2048_chan632_selfcal{0:d}'.format(ii),
+rm -rf ch3oh_1024_chan632_selfcal4.*
+clean(vis=vis, spw='0', imagename='ch3oh_2048_chan632_selfcal4',
       field='W51 Ku',
       weighting='uniform', imsize=[2048,2048], cell=['0.1 arcsec'],
       mode='mfs', threshold='2 mJy', niter=10000,
