cc05e1bf3d6577586f4219f29035d7a72e55ff13
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2015-01-12 10:06:49 +0100

    move continuum_split new code to cont_splits where it was

--- C_Aarray/cont_splits.py
+++ C_Aarray/cont_splits.py
@@ -1,15 +1,27 @@
 vis = '../13A-064.sb28612538.eb29114303.56766.55576449074.ms'
-contvis = 'W51_Cband_Aarray_continuum.ms'
+#contvis = 'W51_Cband_Aarray_continuum.ms'
+#
+#split(vis=vis, outputvis=contvis, spw='0,1,2,3,4,5,6,8,9,10,12,14,16,18,20,21',
+#      field='W51 Ku', width=16)
+#
+#""" May 29 2014: Not yet done """
+#phasecaltable = '../ch3oh/ch3oh_selfcal_phase09'
+#ampcaltable = '../ch3oh/ch3oh_selfcal_ampphase'
+#blcaltable = '../ch3oh/ch3oh_selfcal_blcal'
+#
+#applycal(vis=contvis,
+#         gaintable=[phasecaltable,ampcaltable,blcaltable],
+#         interp='linear',
+#         flagbackup=True) # was False when flagmanager was used
 
-split(vis=vis, outputvis=contvis, spw='0,1,2,3,4,5,6,8,9,10,12,14,16,18,20,21',
-      field='W51 Ku', width=16)
 
-""" May 29 2014: Not yet done """
-phasecaltable = '../ch3oh/ch3oh_selfcal_phase09'
-ampcaltable = '../ch3oh/ch3oh_selfcal_ampphase'
-blcaltable = '../ch3oh/ch3oh_selfcal_blcal'
+vis = '13A-064.sb28612538.eb29114303.56766.55576449074.ms'
+high,low = '0,1,2,3,4,5,6,8','9,10,12,14,16,18,20,21'
 
-applycal(vis=contvis,
-         gaintable=[phasecaltable,ampcaltable,blcaltable],
-         interp='linear',
-         flagbackup=True) # was False when flagmanager was used
+outvis_low = 'W51_CbandAarray_4.2to5.2GHz_continuum.ms'
+outvis_high = 'W51_CbandAarray_5.8to6.7GHz_continuum.ms'
+
+split(vis=vis, outputvis=outvis_high, spw=high,
+      datacolumn='corrected', field='W51 Ku', width=128)
+split(vis=vis, outputvis=outvis_low, spw=low,
+      datacolumn='corrected', field='W51 Ku', width=128)
