66388917fedddd719293802827a67aef27926676
Adam Ginsburg <keflavich@yahoo.com>
2015-02-05 10:12:30 +0100

    merged h2co fix up gridding

--- C_AC/h2co_merge.py
+++ C_AC/h2co_merge.py
@@ -13,7 +13,7 @@ if not os.path.exists(vis_A):
              outfile='pipeline_flags_backup')
     split(vis='../../W51_C_A/13A-064.sb28612538.eb29114303.56766.55576449074.ms',
           outputvis=vis_A, datacolumn='corrected',
-          spw='17', field='W51 Ku', width=4)
+          spw='17', field='W51 Ku', width=1)
 
 vis_C = 'h2co11_Cband_Carray.ms'
 
@@ -23,7 +23,7 @@ if not os.path.exists(vis_C):
              outfile='pipeline_flags_backup')
     split(vis='../../W51_C_C/13A-064.sb21341436.eb23334759.56447.48227415509.ms',
           outputvis=vis_C, datacolumn='corrected',
-          spw='17', field='W51 Ku', width=4)
+          spw='17', field='W51 Ku', width=1)
 
 
 Aphasecaltable = '../../W51_C_A/ch3oh/ch3oh_selfcal_phase09'
@@ -49,7 +49,11 @@ imagename = 'H2CO_11_speccube_contsub_AC_1024_0.1as_uniform_selfcal_dirty'
 os.system('rm -rf {0}.*'.format(imagename))
 clean(vis=[vis_A+".contsub",vis_C+".contsub"],
       imagename=imagename, field='W51 Ku', 
-      mode='channel', 
+      mode='velocity', 
+      start='30km/s',
+      width='0.5km/s',
+      nchan=120,
+      interpolation='linear',
       weighting='uniform', niter=0, spw='0', cell=['0.1 arcsec'],
       imsize=[1024,1024],
       outframe='LSRK',
@@ -66,7 +70,11 @@ imagename = 'H2CO_11_speccube_contsub_AC_1024_0.1as_uniform_selfcal'
 os.system('rm -rf {0}.*'.format(imagename))
 clean(vis=[vis_A+".contsub",vis_C+".contsub"],
       imagename=imagename,field='W51 Ku', 
-      mode='channel', 
+      mode='velocity', 
+      start='30km/s',
+      width='0.5km/s',
+      nchan=120,
+      interpolation='linear',
       weighting='uniform', niter=10000, spw='0', cell=['0.1 arcsec'],
       imsize=[1024,1024],
       outframe='LSRK',
