268d19f415d4caaa603ca82aa07b3c8da53e1f59
Adam Ginsburg <keflavich@yahoo.com>
2015-02-09 08:50:09 +0100

    minor tweaks to reduction scripts & add some analysis stuff
    Was getting these errors:
    2015-02-09 07:38:34     SEVERE  MatrixCleaner::clean() (file /var/rpmbuild/BUILD/casapy421/casapy421-42.1.29047/code/synthesis/MeasurementEquations/MatrixCleaner.cc, line 326)        As Peak of PSF is negative, you should setscales again with a smaller scale size
    which I think means I need to cut the largest-scale multiscale component (?)

---
+++ fnames.py
@@ -0,0 +1,13 @@
+cube_names = {
+    '11_uniform': 'H2CO_11_speccube_contsub_AC_1024_0.1as_uniform_selfcal.image.fits',
+    '11_natural': 'H2CO_11_speccube_contsub_AC_1024_0.1as_natural_selfcal_dirty.image.fits',
+    '22_natural': 'W51Ku_BD_h2co_v30to90_natural_contsub.image.fits',
+    '22_briggs0': 'W51Ku_BD_h2co_v30to90_briggs0_contsub.image.fits',
+}
+
+continua = {
+    #'11_cont_uniform':'',
+    #'22_cont_uniform':'',
+}
+
+fnames = cube_names + continua
--- make_taucubes_all.py
+++ make_taucubes_all.py
@@ -7,7 +7,8 @@ from fnames import fnames
 
 vranges = [(67,71),(42,63),(63,67)]
 
-tc11n = make_taucube(dpath(fnames['11_natural']), )
-tc11u = make_taucube(dpath(fnames['11_uniform']), )
-tc22n = make_taucube(dpath(fnames['22_natural']), )
-tc22b = make_taucube(dpath(fnames['22_briggs0']), )
+TODO CONTINUUM THIS IS A SYNTAX ERROR INTENTIONALLY
+tc11n = make_taucube(dpath(fnames['11_natural']), CONTINUUM )
+tc11u = make_taucube(dpath(fnames['11_uniform']), CONTINUUM )
+tc22n = make_taucube(dpath(fnames['22_natural']), CONTINUUM )
+tc22b = make_taucube(dpath(fnames['22_briggs0']), CONTINUUM )
