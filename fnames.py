af7a9803eb90fb526e605aad24185182e3ef69b8
Adam Ginsburg <keflavich@yahoo.com>
2015-02-11 18:54:22 +0100

    change filenames

--- fnames.py
+++ fnames.py
@@ -7,11 +7,12 @@ cube_names = {
 
 continua = {
     '11_cont_uniform':'H2CO_11_speccube_continuum_AC_1024_0.1as_uniform_selfcal_clean.image.fits',
-    '11_cont_natural':'H2CO_11_speccube_continuum_AC_1024_0.1as_natural_selfcal_clean.image.fits',
+    #'11_cont_natural':'H2CO_11_speccube_continuum_AC_1024_0.1as_natural_selfcal_clean.image.fits',
     '22_cont_natural':'W51Ku_BD_h2co_natural_continuum.image.fits',
     '22_cont_briggs0':'W51Ku_BD_h2co_briggs0_continuum.image.fits',
     '22_cont_2048': 'W51Ku_BDarray_continuum_2048_both_uniform.hires.clean.image.fits',
     '11_cont_low_4096': 'W51C_ACarray_continuum_4096_low_uniform.clean.image.fits',
+    '11_cont_both_2048': 'W51C_ACarray_continuum_2048_both_uniform.clean.image.fits',
 }
 
 fnames = dict(cube_names, **continua)
